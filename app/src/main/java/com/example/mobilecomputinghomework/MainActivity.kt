964b4a10ad2c9bd0a87d09b525db3f53f96445ef
Jerry Korpi <jerry@localhost.localdomain>
2021-01-26 10:32:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobilecomputinghomework/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mobilecomputinghomework
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            Log.d("Lab","Login Button Clicked")
+
+            startActivity(
+                Intent(applicationContext,RemainderList::class.java)
+            )
+        }
+    }
+}
